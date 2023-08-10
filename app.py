ec32f7a2d2ab3821c81f5d8db46bea54df8187a5
Karamelooo <marmoto@hotmail.fr>
2023-08-10 21:08:30 +0200

    ajout interface pygame

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from utils.utils import send_marmiton_query
+import pygame
+from pygame import *
+from io import BytesIO
+from urllib.request import urlopen
 
 def nbr_ingredient_def():
     ingredient_is_setup = False
@@ -101,16 +105,78 @@ def main():
         
         recette_list = send_marmiton_query("-".join(filter_list[0]["ingredients"]),filter_list[3]["nbr_prix"],filter_list[2]["difficulte"],filter_list[1]["temps_passe"])
 
-        # on recupère la list de recettes trouvées
-        i = 1
-        for recette in recette_list:
-            print(f"-- RECETTE N°{i} --\n{recette['recette_name']} avec une note de {recette['recette_note']}\nVoir la recette : {recette['recette_link']}\n")
-            i+=1
 
+        recette_displayed = False
+        scroll = False
+
+        # initiation pygame pour afficher les résultats
+        pygame.init()
+
+        ctx_width = 800
+        ctx_height = 600
+        nb = 0
+        total = len(recette_list)
+        ctx = pygame.display.set_mode((ctx_width, ctx_height))
+        pygame.display.set_caption(f"{total} recettes trouvées")
+
+        font = pygame.font.Font(None, 20)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and scroll is False:
+                    scroll = True
+                    if event.button == 4 and nb > 0:
+                        nb -= 15
+                    elif event.button == 5 and nb < total:
+                        nb += 15
+
+            # génération de l'image
+            ctx.fill((210, 210, 210))
+            x,y = (0,0)
+            w_rect = 100
+            h_rect = 40
+            rect_color = (100,100,100)
+            border_color = (0,0,0)
+            txt_color = (0,0,0)
+            border_size = 1
+            # génération des recettes trouvées
+            if(recette_displayed is False or scroll is True):
+                if(nb+15 > total):
+                    max_i = total
+                else:
+                    max_i = nb+15
+                for index in range(nb, max_i):
+                    recette = recette_list[index]
+                    response = urlopen(recette['recette_image']).read()
+                    image = BytesIO(response)
+                    image = pygame.image.load(image)
+                    image = pygame.transform.scale(image, (100, 50))
+                    texte = f"""-- RECETTE N°{index+1} --
+                    {recette['recette_name']} avec une note de {recette['recette_note']}
+                    Voir la recette : {recette['recette_link']}
+                    """
+                    # rectangle + bordure
+                    pygame.draw.rect(ctx, rect_color, (x, y, w_rect, h_rect))
+                    pygame.draw.rect(ctx, border_color, (x - border_size, y - border_size, w_rect + 2 * border_size, h_rect + 2 * border_size), border_size)
+                    pygame.draw.rect(ctx, border_color, (x - border_size, y - border_size, 1000 + 2 * border_size, h_rect + 2 * border_size), border_size)
+                    # affichage de l'image dans le rectangle si existante
+                    ctx.blit(image, (x, y))
+                    # génération du texte
+                    for ligne in texte.splitlines():
+                        surface_texte = font.render(ligne, True, txt_color)
+                        ctx.blit(surface_texte, (100, y))
+                        pygame.display.flip()
+                        y+=10
+                y+=10
+            recette_displayed = True
+            scroll = False
+
+        pygame.quit()
         
     except Exception as e:
         print("Une erreur s'est produite ! ",e)
-
-
 
 main()
