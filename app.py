6e351001ee48d9e3a4404711ce25eccae2f44ad3
Karamelooo <marmoto@hotmail.fr>
2023-08-11 11:25:28 +0200

    upgrade interface add stars, clickable links

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pygame
 from pygame import *
 from io import BytesIO
 from urllib.request import urlopen
+import webbrowser
+import math
 
 def nbr_ingredient_def():
     ingredient_is_setup = False
@@ -115,13 +117,17 @@ def main():
         ctx_width = 800
         ctx_height = 600
         nb = 0
+        step = 4
         total = len(recette_list)
         ctx = pygame.display.set_mode((ctx_width, ctx_height))
         pygame.display.set_caption(f"{total} recettes trouvées")
-
-        font = pygame.font.Font(None, 20)
-
+        verdana = pygame.font.match_font('verdana')
+        wingdings = pygame.font.match_font('wingdings2')
+        font = pygame.font.Font(verdana, 11)
+        font_stars = pygame.font.Font(wingdings, 17)
         running = True
+        links = []
+        pos_y = []
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -129,35 +135,61 @@ def main():
                 if event.type == pygame.MOUSEBUTTONDOWN and scroll is False:
                     scroll = True
                     if event.button == 4 and nb > 0:
-                        nb -= 15
+                        nb -= step
                     elif event.button == 5 and nb < total:
-                        nb += 15
+                        nb += step
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    print(event.pos) #coordonnées du clique
+                    n_link = 0
+                    for p in pos_y:
+                        if event.pos[1] >= p and event.pos[1] < p+45:
+                            print(p)
+                            webbrowser.open(links[n_link])
+                            break
+                        n_link+=1
 
             # génération de l'image
-            ctx.fill((210, 210, 210))
+            ctx.fill((235, 235, 235))
             x,y = (0,0)
             w_rect = 100
             h_rect = 40
             rect_color = (100,100,100)
             border_color = (0,0,0)
-            txt_color = (0,0,0)
+            txt_color = (30,30,30)
+            stars_color = (255,0,0)
+            blank_stars_color = (0,0,0)
             border_size = 1
             # génération des recettes trouvées
             if(recette_displayed is False or scroll is True):
+                links = []
+                pos_y = []
                 if(nb+15 > total):
                     max_i = total
                 else:
                     max_i = nb+15
                 for index in range(nb, max_i):
                     recette = recette_list[index]
+                    links.append(recette['recette_link'])
+                    pos_y.append(y)
+
                     response = urlopen(recette['recette_image']).read()
                     image = BytesIO(response)
                     image = pygame.image.load(image)
-                    image = pygame.transform.scale(image, (100, 50))
-                    texte = f"""-- RECETTE N°{index+1} --
-                    {recette['recette_name']} avec une note de {recette['recette_note']}
-                    Voir la recette : {recette['recette_link']}
-                    """
+                    image = pygame.transform.scale(image, (100, 40))
+                    note = recette['recette_note'].split('/')
+                    note = math.floor(float(note[0]))
+                    stars = ""
+                    blank_stars = ""
+                    for star in range(0, note):
+                        stars += "ê"
+                    for ok in range(0, 5-note):
+                        blank_stars += "ê"
+                    title = f"""-- RECETTE N°{index+1} --"""
+                    texte = f"""
+{recette['recette_name']} avec une note de {recette['recette_note']}
+Cliquez pour accéder à la recette
+
+"""
                     # rectangle + bordure
                     pygame.draw.rect(ctx, rect_color, (x, y, w_rect, h_rect))
                     pygame.draw.rect(ctx, border_color, (x - border_size, y - border_size, w_rect + 2 * border_size, h_rect + 2 * border_size), border_size)
@@ -165,9 +197,16 @@ def main():
                     # affichage de l'image dans le rectangle si existante
                     ctx.blit(image, (x, y))
                     # génération du texte
+                    y+=5
+                    surface_texte = font.render(title, True, txt_color)
+                    ctx.blit(surface_texte, (105, y))
+                    surface_texte = font_stars.render(stars, True, stars_color)
+                    x_stars = ctx.blit(surface_texte, (220, y)).right
+                    surface_texte = font_stars.render(blank_stars, True, blank_stars_color)
+                    ctx.blit(surface_texte, (x_stars, y))
                     for ligne in texte.splitlines():
                         surface_texte = font.render(ligne, True, txt_color)
-                        ctx.blit(surface_texte, (100, y))
+                        ctx.blit(surface_texte, (105, y))
                         pygame.display.flip()
                         y+=10
                 y+=10
